d2b190318fa0647b74a3b96c2d052b7bd89956b0
Berthe Seydou <seberthe@sqli-1wnwmk2.SQLI>
2018-03-14 09:48:40 +0000

    Commit Before JSON P/B State

--- src/main/java/ma/seydou/jee8/learn/boundaries/CarsResource.java
+++ src/main/java/ma/seydou/jee8/learn/boundaries/CarsResource.java
@@ -3,7 +3,9 @@ package ma.seydou.jee8.learn.boundaries;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -12,6 +14,7 @@ import ma.seydou.jee8.learn.entity.Car;
 
 @Path("cars")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CarsResource {
 
 	@Inject
@@ -20,5 +23,10 @@ public class CarsResource {
 	@GET
 	public List<Car> retrieveCars(){
 		return carManufacturer.retrieveCars();
+	}
+	
+	@POST
+	public void createCar(Specification specification) {
+		carManufacturer.manufactor(specification);
 	}
 }
--- src/main/java/ma/seydou/jee8/learn/boundaries/Specification.java
+++ src/main/java/ma/seydou/jee8/learn/boundaries/Specification.java
@@ -5,19 +5,33 @@ import ma.seydou.jee8.learn.entity.EngineType;
 
 public class Specification {
 
-	private final Color color;
-	private final EngineType engineType;
+	private  Color color;
+	private  EngineType engineType;
 	
 	public Specification(Color color, EngineType engineType) {
 		super();
 		this.color = color;
 		this.engineType = engineType;
 	}
+	
+	public Specification() {
+		super();
+	}
+
 	public Color getColor() {
 		return color;
 	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
 	public EngineType getEngineType() {
 		return engineType;
-	}	
+	}
+
+	public void setEngineType(EngineType engineType) {
+		this.engineType = engineType;
+	}
 	
 }
